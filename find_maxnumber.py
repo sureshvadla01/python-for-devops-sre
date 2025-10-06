dd94f68b977f25cafffc926cb64cca2748587364
Suresh V <suresh57758@gmail.com>
2025-10-06 08:22:47 -0400

    scripts

---
+++ find_maxnumber.py
@@ -0,0 +1,6 @@
+#Find the Largest of Three Numbers
+
+def largerst(x,y,z):
+    return max(x,y,z)
+
+print(largerst(244,3,45))
---
+++ oddoreven_number.py
@@ -0,0 +1,24 @@
+#Write a program that checks whether a number entered by the user is even or odd.
+
+#number=input('please enter a number')
+
+number=4
+
+
+if number % 2 == 0:
+    print('even number')
+elif number %2 != 0: 
+    print('odd number')
+else:
+    print('enter only numbers')
+        
+
+number=3
+
+
+if number % 2 == 0:
+    print('even number')
+elif number %2 != 0: 
+    print('odd number')
+else:
+    print('enter only numbers')
