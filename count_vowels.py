ed219f9714bc3427a7fa83baad2cb8ecf37343b0
Suresh V <suresh57758@gmail.com>
2025-10-06 08:22:47 -0400

    scripts

---
+++ count_vowels.py
@@ -0,0 +1,12 @@
+#count_vowels("Python Programming")
+
+
+def count_vowels(input_string):
+    vowels = "aeiouAEIOU"
+    count = 0
+    for char in input_string:
+        if char in vowels:
+            count += 1
+    return count
+
+print(count_vowels("Python Programming"))
